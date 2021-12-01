c7ccc93bc1765e2dc908dc1aa832ed6777673d90
Vladislav Kyrachenko <jowissa@MacBook-Pro-Vladislav.local>
2021-12-01 17:57:18 +0200

    jsCalc

---
+++ calc.js
@@ -0,0 +1,67 @@
+let a = '';
+let b = '';
+let sign = '';
+let finish = false;
+
+const digit = ['0','1', '2', '3', '4', '5', '6', '7', '8', '9', '.'];
+const action = ['-', '+', 'x', '/'];
+
+const out = document.querySelector('.calcScreen p');
+
+function clearAll(){
+    a = '';
+    b = '';
+    sign = '';
+    finish = false;
+    out.textContent = 0;
+}
+
+document.querySelector('.all-clear').onclick = clearAll;
+
+document.querySelector('.btns').onclick = (event) => {
+    if(event.target.classList.contains('all-clear')) return;
+
+    out.textContent = '';
+
+    const key = event.target.textContent;
+    
+    if(digit.includes(key)) {
+        if(b === '' & sign === ''){
+        a += key;
+
+        out.textContent = a;
+        }
+        else if (a !== '' && b !== '' && finish){
+          b = key;
+          finish = false;
+          out.textContent = b;
+        }
+        else {
+            b += key;
+            out.textContent = b
+
+        }
+        return;
+    }
+    if(action.includes(key)) {
+        sign = key;
+        out.textContent = sign;
+        return;
+    }
+
+    if(key === '='){
+        if (b === '') b = a
+        switch(sign){
+            case '+': a = (+a) + (+b);
+                  break;
+            case '-': a = a - b;
+                  break;  
+            case 'x': a = a * b;
+                  break;  
+            case '/': a = a / b;
+                 break;     
+        }
+        finish = true;
+        out.textContent = a;
+    }
+}
